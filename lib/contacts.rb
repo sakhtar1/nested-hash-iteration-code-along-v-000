b8df1b1d5ac744bd288a116cc7ec4c06654a47cb
Sharmina Akhtar <sakht318@gmail.com>
2017-11-14 16:33:28 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,12 +14,12 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts)
-    contacts.each do |person, contact_details_hash|
-      contact_details_hash.each do |attribute, data|
+  def remove_strawberry(contacts)
+    contacts.each do |person, data|
+      data.each do |attribute, value|
         if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+            value.delete_if {|ice_cream| ice_cream == "strawberry"}
+          end
         end
       end
-    end
-end
+  end
